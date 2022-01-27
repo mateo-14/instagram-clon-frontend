c4016f5fd2d094a2db900a3a26c4c411dc8f8701
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-01-27 15:52:00 -0300

    Add icons

---
+++ src/components/common/Icons/CommentIcon.js
@@ -0,0 +1,13 @@
+const CommentIcon = ({ className }) => (
+  <svg aria-label="Comment" className={className} role="img" viewBox="0 0 24 24">
+    <path
+      d="M20.656 17.008a9.993 9.993 0 10-3.59 3.615L22 22z"
+      fill="none"
+      stroke="currentColor"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    ></path>
+  </svg>
+);
+
+export default CommentIcon;
---
+++ src/components/common/Icons/OutlineHomeIcon.js
@@ -0,0 +1,20 @@
+const OutlineHomeIcon = ({ className }) => (
+  <svg
+    aria-label="Home"
+    className={className}
+    color="#262626"
+    fill="none"
+    role="img"
+    viewBox="0 0 24 24"
+  >
+    <path
+      d="M9.005 16.545a2.997 2.997 0 012.997-2.997h0A2.997 2.997 0 0115 16.545V22h7V11.543L12 2 2 11.543V22h7.005z"
+      fill="none"
+      stroke="currentColor"
+      strokeLinejoin="round"
+      strokeWidth="2"
+    ></path>
+  </svg>
+);
+
+export default OutlineHomeIcon;
