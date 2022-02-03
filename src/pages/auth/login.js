056655b8998e8b31205d4de5757ee25bff78a5f5
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-02-03 17:33:19 -0300

    Implement auth forms logic

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -3,17 +3,63 @@ import Link from 'next/link';
 import Input from 'components/common/Input';
 import Button from 'components/common/Button';
 import AuthPage from 'components/AuthPage';
-import { useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import useAuth from 'hooks/useAuth';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const schema = yup
+  .object({
+    username: yup.string().required('Username is required').trim(),
+    password: yup.string().required('Password is required'),
+  })
+  .required();
 
 export default function Signin() {
-  const ref = useRef();
+  const { login, isLogged } = useAuth();
+  const router = useRouter();
+  const { register, handleSubmit, formState, setError } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onChange',
+  });
+  const { errors } = formState;
+
+  const onSubmit = async (data) => {
+    const res = await login(data);
+    if (!res.error) return;
+
+    if (res.errors) {
+      for (const field in res.errors) {
+        setError(field, { message: res.errors[field] });
+      }
+    } else setError('submit', { message: res.error });
+  };
+
+  useEffect(() => {
+    if (isLogged) {
+      if (formState.isSubmitted) router.push('/');
+      else router.replace('/');
+    }   
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLogged, router]);
+
   return (
     <AuthPage.Layout title={'Login'}>
       <AuthPage.MainSection>
-        <form className={sharedStyles.form}>
-          <Input placeholder="Username"></Input>
-          <Input placeholder="Password" masked={true}></Input>
-          <Button className={sharedStyles.button} disabled onClick={() => alert('a')}>
+        <form className={sharedStyles.form} onSubmit={handleSubmit(onSubmit)}>
+          <div className={sharedStyles.inputWrapper}>
+            <Input placeholder="Username" {...register('username')}></Input>
+            <p className={sharedStyles.errorText}>{errors?.username?.message}</p>
+          </div>
+          <div className={sharedStyles.inputWrapper}>
+            <Input placeholder="Password" {...register('password')} masked={true}></Input>
+            <p className={sharedStyles.errorText}>{errors?.password?.message}</p>
+          </div>
+
+          <p className={sharedStyles.errorText}>{errors?.submit?.message}</p>
+          <Button className={sharedStyles.button} disabled={!formState.isValid}>
             Log in
           </Button>
         </form>
--- src/pages/auth/signup.js
+++ src/pages/auth/signup.js
@@ -4,17 +4,77 @@ import Input from 'components/common/Input';
 import Button from 'components/common/Button';
 import AuthPage from 'components/AuthPage';
 import styles from 'styles/auth/Signup.module.css';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import useAuth from 'hooks/useAuth';
+import { useRouter } from 'next/router';
+
+const schema = yup
+  .object({
+    username: yup
+      .string()
+      .trim()
+      .required('Username is required')
+      .max(25, 'Username must be 3 to 25 characters long')
+      .min(3, 'Username must be 3 to 25 characters long'),
+    displayName: yup.string().trim().optional().max(30, 'Name must be less than 30 characters'),
+    password: yup
+      .string()
+      .required('Password is required')
+      .max(30, 'Password must be 4 to 25 characters long')
+      .min(4, 'Password must be 4 to 25 characters long'),
+  })
+  .required();
 
 export default function Signup() {
+  const router = useRouter();
+  const { signUp, isLogged } = useAuth();
+  const { register, handleSubmit, formState, setError } = useForm({
+    resolver: yupResolver(schema),
+    mode: 'onChange',
+  });
+  const { errors } = formState;
+
+  const onSubmit = async (data) => {
+    const res = await signUp(data);
+    if (!res.error) return;
+
+    if (res.errors) {
+      for (const field in res.errors) {
+        setError(field, { message: res.errors[field] });
+      }
+    } else setError('submit', { message: res.error });
+  };
+
+  useEffect(() => {
+    if (isLogged) {
+      if (formState.isSubmitted) router.push('/');
+      else router.replace('/');
+    }   
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isLogged, router]);
+
   return (
     <AuthPage.Layout title={'Login'}>
       <AuthPage.MainSection>
         <p className={styles.infoText}>Sign up to see photos and videos from your friends.</p>
-        <form className={sharedStyles.form}>
-          <Input placeholder="Username"></Input>
-          <Input placeholder="Full Name"></Input>
-          <Input placeholder="Password" masked={true}></Input>
-          <Button className={sharedStyles.button} disabled>
+        <form className={sharedStyles.form} onSubmit={handleSubmit(onSubmit)}>
+          <div className={sharedStyles.inputWrapper}>
+            <Input placeholder="Username" {...register('username')}></Input>
+            <p className={sharedStyles.errorText}>{errors?.username?.message}</p>
+          </div>
+          <div className={sharedStyles.inputWrapper}>
+            <Input placeholder={'Full name'} {...register('displayName')}></Input>
+            <p className={sharedStyles.errorText}>{errors?.displayName?.message}</p>
+          </div>
+          <div className={sharedStyles.inputWrapper}>
+            <Input placeholder="Password" {...register('password')} masked={true}></Input>
+            <p className={sharedStyles.errorText}>{errors?.password?.message}</p>
+          </div>
+          <p className={sharedStyles.errorText}>{errors?.submit?.message}</p>
+
+          <Button className={sharedStyles.button} disabled={!formState.isValid}>
             Sign up
           </Button>
         </form>
