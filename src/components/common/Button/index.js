2ca9877fb43b0fd5a6614028c432c386878b3d63
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-02-16 22:03:23 -0300

    Add button types

--- src/components/common/Button/index.js
+++ src/components/common/Button/index.js
@@ -1,8 +1,8 @@
 import styles from './Button.module.css';
 
-const Button = ({ children, className, onClick, disabled = false }) => (
+const Button = ({ children, className, onClick, disabled = false, type = 'solid' }) => (
   <button
-    className={`${styles.button} ${className ? className : ''}`}
+    className={`${styles.button} ${styles[type]} ${className ? className : ''}`}
     disabled={disabled}
     onClick={onClick}
   >
