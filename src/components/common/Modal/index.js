3174b6e0d8e2584daee302f3acb7fa3c71b9e4d2
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-02-18 16:51:15 -0300

    Update modal

--- src/components/common/Modal/index.js
+++ src/components/common/Modal/index.js
@@ -1,9 +1,16 @@
+import classNames from 'classnames';
+import useOnClickOutside from 'hooks/useOnClickOutside';
+import { useRef } from 'react';
 import styles from './Modal.module.css';
 
 export default function Modal({ children, className, show, onClose, showCloseButton }) {
+  const ref = useRef();
+  useOnClickOutside(ref, onClose);
   return show ? (
     <div className={styles.background}>
-      <div className={`${styles.modal} ${className || ''}`}>{children}</div>
+      <div className={className} ref={ref}>
+        {children}
+      </div>
       {showCloseButton && (
         <button className={styles.closeButton} onClick={onClose}>
           <svg aria-label="Close" viewBox="0 0 24 24" color="currentColor">
