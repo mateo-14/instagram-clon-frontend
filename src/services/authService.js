c617ef8f7fb11d5a680c46dae111e39879e1cc05
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-03-27 21:58:48 -0300

    Update getToken function

--- src/services/authService.js
+++ src/services/authService.js
@@ -23,16 +23,18 @@ export function signUp(data) {
 }
 
 export function auth() {
-  const token = getToken();
+  return new Promise((resolve, reject) => {
+    const token = getToken();
+    if (!token) return reject({ error: 'Not token' });
 
-  return axios
-    .get(`${ENDPOINT}`, { headers: { authorization: `Bearer ${token}` } })
-    .then(({ data }) => data)
-    .catch(({ response }) => ({ error: response?.data }));
+    axios
+      .get(`${ENDPOINT}`, { headers: { authorization: `Bearer ${token}` } })
+      .then(({ data }) => resolve(data))
+      .catch(({ response }) => reject({ error: response?.data }));
+  });
 }
 
 export function getToken() {
   const token = localStorage.getItem('token');
-  if (!token) throw new Error('Token not found');
   return token;
 }
