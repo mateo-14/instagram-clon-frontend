76175628c143094a7e0d9b4a0c551155a88571c0
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-03-27 21:57:06 -0300

    Implement classNames, add id prop

--- src/components/common/Input/index.js
+++ src/components/common/Input/index.js
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import PropTypes from 'prop-types';
 import { forwardRef } from 'react';
 import styles from './Input.module.css';
@@ -5,15 +6,16 @@ import styles from './Input.module.css';
 /**
  * @type React.ForwardRefRenderFunction<React.FunctionComponent, InputPropTypes>
  */
-const Input = forwardRef(({ masked, placeholder, value, onChange, className, name }, ref) => (
+const Input = forwardRef(({ masked, placeholder, value, onChange, className, name, id }, ref) => (
   <input
-    className={`${styles.input} ${className ? className : ''}`}
+    className={classNames(styles.input, className)}
     placeholder={placeholder}
     onChange={onChange}
     ref={ref}
     value={value}
     type={masked ? 'password' : 'text'}
     name={name}
+    id={id}
   ></input>
 ));
 
@@ -23,7 +25,7 @@ const InputPropTypes = {
   value: PropTypes.string,
   onChange: PropTypes.func,
   className: PropTypes.string,
-  name: PropTypes.string
+  name: PropTypes.string,
 };
 Input.propTypes = InputPropTypes;
 Input.displayName = 'Input';
