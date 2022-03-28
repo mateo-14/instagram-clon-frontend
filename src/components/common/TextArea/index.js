85050c59f4d22214ea9dabd9c3f16bb4dc1f078d
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-03-27 21:57:36 -0300

    Add classNames to TextArea

--- src/components/common/TextArea/index.js
+++ src/components/common/TextArea/index.js
@@ -1,9 +1,10 @@
+import classNames from 'classnames';
 import TextareaAutosize from 'react-textarea-autosize';
 import styles from './TextArea.module.css';
 
 const TextArea = ({ placeholder, className, maxRows, onChange, value, disabled }) => (
   <TextareaAutosize
-    className={`${styles.textarea} ${className || ''}`}
+    className={classNames(styles.textarea, className || '')}
     placeholder={placeholder}
     maxRows={maxRows}
     onChange={onChange}
