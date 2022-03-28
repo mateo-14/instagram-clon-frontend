799ae3a14ce5dd608dfb58f2ae9fe66728c54ef7
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-03-27 21:59:15 -0300

    Change getToken implementation

--- src/services/postsServices.js
+++ src/services/postsServices.js
@@ -3,37 +3,74 @@ import { getToken } from './authService';
 const ENDPOINT = `${process.env.NEXT_PUBLIC_API_URL}/posts`;
 
 export function createPost(file, caption) {
-  const token = getToken();
+  return new Promise((resolve, reject) => {
+    const token = getToken();
+    if (!token) return reject({ error: 'Not token' });
 
-  const formData = new FormData();
-  formData.set('images', file);
-  formData.set('text', caption);
-  return axios
-    .post(ENDPOINT, formData, {
-      headers: { authorization: `Bearer ${token}`, 'Content-Type': 'multipart/form-data' },
-    })
-    .then(({ data }) => data)
-    .catch(({ response }) => reject({ error: response?.data }));
+    const formData = new FormData();
+    formData.set('images', file);
+    formData.set('text', caption);
+    axios
+      .post(ENDPOINT, formData, {
+        headers: { authorization: `Bearer ${token}`, 'Content-Type': 'multipart/form-data' },
+      })
+      .then(({ data }) => resolve(data))
+      .catch(({ response }) => reject({ error: response?.data }));
+  });
 }
 
 export function getFeed(last) {
-  const token = getToken();
+  return new Promise((resolve, reject) => {
+    const token = getToken();
+    if (!token) return reject({ error: 'Not token' });
 
-  return axios
-    .get(`${ENDPOINT}/feed${last ? '?last=' + last : ''}`, {
-      headers: { authorization: `Bearer ${token}` },
-    })
-    .then(({ data }) => data)
-    .catch(({ response }) => ({ error: response?.data }));
+    axios
+      .get(`${ENDPOINT}/feed${last ? '?last=' + last : ''}`, {
+        headers: { authorization: `Bearer ${token}` },
+      })
+      .then(({ data }) => resolve(data))
+      .catch(({ response }) => reject({ error: response?.data }));
+  });
 }
 
 export function getPost(id) {
-  const token = getToken();
+  return new Promise((resolve, reject) => {
+    const token = getToken();
+    if (!token) return reject({ error: 'Not token' });
 
-  return axios
-    .get(`${ENDPOINT}/${id}`, {
-      headers: { authorization: `Bearer ${token}` },
-    })
-    .then(({ data }) => data)
-    .catch(({ response }) => ({ error: response?.data }));
+    axios
+      .get(`${ENDPOINT}/${id}`, {
+        headers: { authorization: `Bearer ${token}` },
+      })
+      .then(({ data }) => resolve(data))
+      .catch(({ response }) => reject({ error: response?.data }));
+  });
+}
+
+export function addLike(id) {
+  return new Promise((resolve, reject) => {
+    const token = getToken();
+    if (!token) return reject({ error: 'Not token' });
+
+    axios
+      .put(`${ENDPOINT}/${id}/likes`, null, {
+        headers: { authorization: `Bearer ${token}` },
+      })
+      .then(({ data }) => resolve(data))
+      .catch(({ response }) => reject({ error: response?.data }));
+  });
+}
+
+export function removeLike(id) {
+  return new Promise((resolve, reject) => {
+    const token = getToken();
+    if (!token) return reject({ error: 'Not token' });
+
+    axios
+      .delete(`${ENDPOINT}/${id}/likes`, {
+        headers: { authorization: `Bearer ${token}` },
+      })
+      .then(({ data }) => resolve(data))
+      .catch(({ response }) => reject({ error: response?.data }));
+  });
 }
