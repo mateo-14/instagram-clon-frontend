a64ef00bfe191136719ac2aef721a12d74afb626
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-06-30 00:03:18 -0300

    Fix cropper aspect ratio

--- src/components/common/ImageCropper/index.jsx
+++ src/components/common/ImageCropper/index.jsx
@@ -1,19 +1,19 @@
-import classNames from 'classnames';
-import { useEffect, useRef, useState } from 'react';
-import styles from './ImageCropper.module.css';
-import { Cropper } from './Cropper';
+import classNames from 'classnames'
+import { useEffect, useRef, useState } from 'react'
+import styles from './ImageCropper.module.css'
+import { Cropper } from './Cropper'
 
 export default function ImageCropper({
   zoom = 0,
   aspectRatio = 1,
   src,
   cropperRef: cropperRefProp,
-  positionRef,
+  positionRef
 }) {
-  const imgRef = useRef();
-  const cropperElemRef = useRef();
-  const cropperRef = useRef();
-  const [isCropping, setIsCropping] = useState(false);
+  const imgRef = useRef()
+  const cropperElemRef = useRef()
+  const cropperRef = useRef()
+  const [isCropping, setIsCropping] = useState(false)
 
   useEffect(() => {
     const cropper = new Cropper(
@@ -23,40 +23,44 @@ export default function ImageCropper({
 
       positionRef.current,
       {
-        onCropEnd: (data) => {
-          positionRef.current = { x: data.x, y: data.y, zoom: data.zoom };
-          setIsCropping(false);
+        onCropEnd: data => {
+          positionRef.current = { x: data.x, y: data.y, zoom: data.zoom }
+          setIsCropping(false)
         },
-        onCropStart: () => setIsCropping(true),
+        onCropStart: () => setIsCropping(true)
       }
-    );
-    cropperRef.current = cropper;
-    cropperRefProp.current = cropper;
-    return () => cropper.destroy();
-  }, [imgRef, cropperRef]);
+    )
+    cropperRef.current = cropper
+    cropperRefProp.current = cropper
+    return () => cropper.destroy()
+  }, [imgRef, cropperRef])
 
   useEffect(() => {
-    if (!cropperRef.current) return;
-    cropperRef.current.setAspectRatio(aspectRatio);
-  }, [aspectRatio]);
+    if (!cropperRef.current) return
+    cropperRef.current.setAspectRatio(aspectRatio)
+  }, [aspectRatio])
 
   useEffect(() => {
-    if (!cropperRef.current) return;
+    if (!cropperRef.current) return
 
-    const animRequest = cropperRef.current.setZoom(zoom);
-    return () => cancelAnimationFrame(animRequest);
-  }, [zoom]);
+    const animRequest = cropperRef.current.setZoom(zoom)
+    return () => cancelAnimationFrame(animRequest)
+  }, [zoom])
 
   return (
     <div className={styles.container}>
       <div
         className={styles.cropper}
         ref={cropperElemRef}
-        style={{ aspectRatio: aspectRatio.toString() }}
+        style={{
+          aspectRatio: aspectRatio.toString(),
+          width: aspectRatio >= 1 ? '100%' : 'auto',
+          height: aspectRatio <= 1 ? '100%' : 'auto'
+        }}
       >
         <img src={src} alt="img" className={styles.img} ref={imgRef} />
         <div className={classNames(styles.grid, { [styles.visible]: isCropping })} />
       </div>
     </div>
-  );
+  )
 }
