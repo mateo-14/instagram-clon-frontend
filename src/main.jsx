a9dde471174bdf634231696e2c36550b61ace21a
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-06-29 23:17:11 -0300

    Add EventEmitter and Events constants

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './globals.css';
-import { BrowserRouter } from 'react-router-dom';
-import AuthProvider from 'context/AuthContext';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './globals.css'
+import { BrowserRouter } from 'react-router-dom'
+import AuthProvider from 'context/AuthContext'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+import { EventEmitter } from './events/EventEmitter'
 
 export const queryClient = new QueryClient({
-  defaultOptions: { queries: { refetchOnWindowFocus: false } },
-});
+  defaultOptions: { queries: { refetchOnWindowFocus: false } }
+})
+
+export const eventEmitter = new EventEmitter()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -22,4 +25,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
-);
+)
