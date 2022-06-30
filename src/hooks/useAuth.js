1149832b15b492f59d115aa38dfb168a90b168f5
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-06-29 23:27:29 -0300

    Clear react-query when I logout

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,22 +1,27 @@
-import { AuthContext } from 'context/AuthContext';
-import { useContext, useEffect } from 'react';
-import * as authService from 'services/authService';
-import { useNavigate } from 'react-router-dom';
-import { getUserById } from 'services/usersService';
-import { useQuery } from 'react-query';
+import { AuthContext } from 'context/AuthContext'
+import { useContext, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { getUserById } from 'services/usersService'
+import { useQuery } from 'react-query'
+import { queryClient } from 'src/main'
 
 export default function useAuth(redirect = true) {
-  const { state, logout, login } = useContext(AuthContext);
-  const { isLogged, isLoading, userId } = state;
-  const navigate = useNavigate();
+  const { state, logout, login } = useContext(AuthContext)
+  const { isLogged, isLoading, userId } = state
+  const navigate = useNavigate()
 
   const { data } = useQuery(['users', userId], () => getUserById(userId), {
-    enabled: !!userId,
-  });
+    enabled: !!userId
+  })
 
+  const logoutAndClear = () => {
+    logout()
+    queryClient.clear()
+  }
+  
   useEffect(() => {
-    if (redirect && !isLoading && !isLogged) navigate('/accounts/login');
-  }, [redirect, isLogged, isLoading, navigate]);
+    if (redirect && !isLoading && !isLogged) navigate('/accounts/login')
+  }, [redirect, isLogged, isLoading, navigate])
 
-  return { isLogged, isLoading, data, login, logout };
+  return { isLogged, isLoading, data, login, logout: logoutAndClear }
 }
