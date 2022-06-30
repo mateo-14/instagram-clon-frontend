dc1022d8ae13487161c929702e9766e0fbad7ab7
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2022-06-30 13:20:04 -0300

    Remove console.log

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -55,7 +55,6 @@ export default function Home() {
 
   useEffect(() => {
     const onCreatePost = post => {
-      console.log(post)
       queryClient.setQueryData(['posts', 'feed'], ({ pages, pageParams }) => {
         return {
           pageParams,
