12e520c2f2432d09424cdf86db4ecfff0c3973d9
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2023-06-24 21:27:56 -0300

    Update dependencies, delete @postModal  from app

--- src/app/(accounts)/accounts/layout.tsx
+++ src/app/(accounts)/accounts/layout.tsx
@@ -20,20 +20,3 @@ export default function AuthLayout ({ children, extra }: { children: React.React
       </main>
   )
 }
-
-/* const MainSection = ({ children }: { children: React.ReactNode }): JSX.Element => (
-  <div className={styles.mainSection}>
-    <header className={styles.header}>
-      <div className={styles.logoWrapper} aria-label="Instagram logo">
-        <Image src="instagram-logo.svg" alt="Instagram"></Image>
-      </div>s
-    </header>
-    {children}
-  </div>
-)
-
-const ExtraSection = ({ children }: { children: React.ReactNode }): JSX.Element => <div className={styles.extraSection}>{children}</div>
-
-const AuthPage = { Layout, MainSection, ExtraSection }
-export default AuthPage
- */
